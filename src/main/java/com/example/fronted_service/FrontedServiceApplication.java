c688ee3aae10095026fbcc4b89426474c6718f92
BryamCode <bryamfernandezuriarte22@gmail.com>
2026-02-22 15:28:20 -0500

    Creacion del servicio de fronted

---
+++ src/main/java/com/example/fronted_service/FrontedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.fronted_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontedServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontedServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fronted_service/FrontedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fronted_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontedServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
